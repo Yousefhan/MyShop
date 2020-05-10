1b1665a85ade55e6c885380c353162fff6364146
Yousefhan <yousef.hanafi95@gmail.com>
2020-05-10 15:55:00 +0300

    Product model InMemory storage

---
+++ MyShop/MyShop.Core/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class Product
+    {
+        public string Id { set; get; }
+        [StringLength(20)]
+        [DisplayName("Product Name")]
+        public string Name { set; get; }
+        public string Description { set; get; }
+        [Range(0,1000)]
+        public decimal Price { set; get; }
+        public string Category { set; get;}
+        public string Image { set; get; }
+
+        public Product()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+    }
+}
