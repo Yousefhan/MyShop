1fdb7333651142ab0177809c65240756522249af
Yousefhan <yousef.hanafi95@gmail.com>
2020-05-27 13:55:44 +0300

    Product listing

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using MyShop.DataAccess.SQL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +11,28 @@ namespace MyShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepository<Product> Context;
+        IRepository<ProductCategory> productCategories;
+        public HomeController()
+        {
+            DataContext DContext = new DataContext();
+            Context = new SQLRepository<Product>(DContext);
+            productCategories = new SQLRepository<ProductCategory>(DContext);
+        }
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = Context.Collection().ToList();
+            return View(products);
         }
-
+        public ActionResult Details(string Id)
+        {
+            Product product = Context.Find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
